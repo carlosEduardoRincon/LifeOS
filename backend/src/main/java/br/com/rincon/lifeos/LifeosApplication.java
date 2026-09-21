fd4f495ec2a37326beebf1027ecda28643f9ae05
carlosEduardoRincon <carloscaca147@gmail.com>
2026-09-21 19:58:46 -0300

    feat: first commmit, spring boot structure

---
+++ backend/src/main/java/br/com/rincon/lifeos/LifeosApplication.java
@@ -0,0 +1,13 @@
+package br.com.rincon.lifeos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeosApplication.class, args);
+	}
+
+}
